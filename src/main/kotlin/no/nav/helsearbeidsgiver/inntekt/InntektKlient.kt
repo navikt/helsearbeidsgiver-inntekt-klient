867986735066afa73246a254df14bc315f427ce7
Morten Laukvik <morten.laukvik@nav.no>
2022-10-10 10:03:30 +0200

    Ktlint fix

--- src/main/kotlin/no/nav/helsearbeidsgiver/inntekt/InntektKlient.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntekt/InntektKlient.kt
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter
 class InntektKlient(
     private val baseUrl: String,
     private val stsClient: AccessTokenProvider,
-    private val httpClient: HttpClient,
+    private val httpClient: HttpClient
 ) {
 
     suspend fun hentInntektListe(
