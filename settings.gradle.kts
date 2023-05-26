b28151437546c9f26a97deebda19319683e0a5f3
Mikael Bjerga <mikael.bjerga@nav.no>
2023-05-26 08:20:24 +0200

    Rydd i byggfil

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "inntekt-klient"
 
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        val kotlinterVersion: String by settings
+
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+        id("org.jmailen.kotlinter") version kotlinterVersion
+    }
+}
