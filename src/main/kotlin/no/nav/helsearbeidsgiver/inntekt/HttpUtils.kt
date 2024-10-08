76468fa981efc91f2368693dba7c0551a36f0ab0
Morten Byhring <morten.byhring@nav.no>
2024-10-08 13:54:10 +0200

    default timeout verdier for httpklient

--- src/main/kotlin/no/nav/helsearbeidsgiver/inntekt/HttpUtils.kt
+++ src/main/kotlin/no/nav/helsearbeidsgiver/inntekt/HttpUtils.kt
@@ -3,6 +3,7 @@ package no.nav.helsearbeidsgiver.inntekt
 import io.ktor.client.HttpClient
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.apache5.Apache5
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import no.nav.helsearbeidsgiver.utils.json.jsonConfig
@@ -15,5 +16,10 @@ internal fun HttpClientConfig<*>.configure() {
 
     install(ContentNegotiation) {
         json(jsonConfig)
+    }
+    install(HttpTimeout) {
+        requestTimeoutMillis = 5000
+        connectTimeoutMillis = 2000
+        socketTimeoutMillis = 2000
     }
 }
